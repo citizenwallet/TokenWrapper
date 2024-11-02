aa9d9402a8324d1fd1998fd065142f9858aa3636
Alex Collette <alexcollette@hotmail.com>
2024-11-02 17:47:19 +0100

    :sparkles: add transfer fct with commission

--- src/EurB.sol
+++ src/EurB.sol
@@ -9,10 +9,6 @@ import {Storage} from "./Storage.sol";
 
 contract EurB is ERC20Wrapper, Ownable, Storage {
     /* //////////////////////////////////////////////////////////////
-                                CONSTANTS
-    ////////////////////////////////////////////////////////////// */
-
-    /* //////////////////////////////////////////////////////////////
                                 EVENTS
     ////////////////////////////////////////////////////////////// */
 
@@ -24,11 +20,56 @@ contract EurB is ERC20Wrapper, Ownable, Storage {
                                 CONSTRUCTOR
     ////////////////////////////////////////////////////////////// */
 
-    constructor(IERC20 underlyingToken, address treasury_)
+    constructor(IERC20 underlyingToken, address treasury_, address cardFactory_)
         ERC20Wrapper(underlyingToken)
         ERC20("EuroBrussels", "EURB")
         Ownable(msg.sender)
     {
         treasury = treasury_;
+        cardFactory = ICardFactory(cardFactory_);
+    }
+
+    /* //////////////////////////////////////////////////////////////
+                                LOGIC
+    ////////////////////////////////////////////////////////////// */
+
+    /**
+     * @notice Will set a new treasury.
+     * @param treasury_ The new treasury address.
+     */
+    function setTreasury(address treasury_) external onlyOwner {
+        treasury = treasury_;
+    }
+
+    /**
+     * @notice Moves an amount of tokens from the caller's account to "to".
+     * @param to The address the tokens are sent to.
+     * @param amount The amount of tokens transferred.
+     * @return A boolean value indicating wether the operation succeeded.
+     */
+    function transfer(address to, uint256 amount) public override returns (bool success) {
+        // Do initial transfer for full amount.
+        _transfer(msg.sender, to, amount);
+
+        // Check if commission has to be paid, use try-catch pattern to not block transfers in case
+        // logic fails in the Card Factory or Commission Hook Module.
+        address commissionHookModule;
+        try cardFactory.COMMISSION_HOOK_MODULE() returns (address commissionHookModule_) {
+            commissionHookModule = commissionHookModule;
+        } catch {}
+
+        // If CommissionHookModule is enabled, get recipients and rates and transfer corresponding
+        // amount from initial "amount" receiver ("to"), to commission beneficiary.
+        try ISafe(msg.sender).isModuleEnabled(commissionHookModule) returns (bool enabled) {
+            if (enabled == true) {
+                (address[] memory recipients, uint256[] memory rates) = ISafe(msg.sender).getCommissionInfo(to);
+                for (uint256 i; i < recipients.length; ++i) {
+                    uint256 commission = amount.mulDivDown(rates[i], BIPS);
+                    _transfer(to, recipients[i], commission);
+                }
+            }
+        } catch {}
+
+        return true;
     }
 }
--- src/Storage.sol
+++ src/Storage.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.22;
 
 contract Storage {
+    /* //////////////////////////////////////////////////////////////
+                                CONSTANTS
+    ////////////////////////////////////////////////////////////// */
+
+    uint256 internal constant BIPS = 10_000;
+
+    /* //////////////////////////////////////////////////////////////
+                                STORAGE
+    ////////////////////////////////////////////////////////////// */
+
+    // Would work with a registry here to access hookModules (others added and change in logic)
+    ICardFactory public cardFactory;
     address public treasury;
 }

f28374e9d51b701318f0e7fb755b3e9e8e2a9c1d
Alex Collette <alexcollette@hotmail.com>
2024-12-02 19:30:13 +0100

    :white_check_mark: add tests for fullWithdraw

---
+++ test/fork/AaveLocker/CollectYield.fork.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {AaveLocker_Fork_Test} from "./_AaveLocker.fork.t.sol";
+import {IPool} from "../../utils/interfaces/aave/IPool.sol";
+
+/**
+ * @notice Fork tests for the function "collectYield" of contract "AaveLocker".
+ */
+contract CollectYield_AaveLocker_Fork_Test is AaveLocker_Fork_Test {
+    /* ///////////////////////////////////////////////////////////////
+                              SETUP
+    /////////////////////////////////////////////////////////////// */
+
+    function setUp() public override {
+        AaveLocker_Fork_Test.setUp();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              TESTS
+    //////////////////////////////////////////////////////////////*/
+    function testFork_Success_collectYield(uint256 depositAmount) public {
+        // Given: Deposit a certain amount.
+        depositAmount = bound(depositAmount, 1e18, 10_000 * 1e18);
+        vm.prank(EUREFund);
+        EURE.transfer(address(EURB), depositAmount);
+
+        vm.startPrank(address(EURB));
+        EURE.approve(address(AAVE_LOCKER), depositAmount);
+        AAVE_LOCKER.deposit(address(EURE), depositAmount);
+
+        vm.warp(block.timestamp + 365 days);
+        uint256 initTotalValue = AAVE_LOCKER.getTotalValue(address(EURE));
+
+        // When: Calling collectYield.
+        uint256 yield = AAVE_LOCKER.collectYield(address(EURE));
+        uint256 expectedYield = initTotalValue - depositAmount;
+        assertEq(expectedYield, yield);
+        assertEq(EURE.balanceOf(address(EURB)), yield);
+
+        vm.stopPrank();
+    }
+}
---
+++ test/fork/AaveLocker/FullWithdraw.fuzz.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {AaveLocker_Fork_Test} from "./_AaveLocker.fork.t.sol";
+import {IPool} from "../../utils/interfaces/aave/IPool.sol";
+
+/**
+ * @notice Fork tests for the function "fullWithdraw" of contract "AaveLocker".
+ */
+contract FullWithdraw_AaveLocker_Fork_Test is AaveLocker_Fork_Test {
+    /* ///////////////////////////////////////////////////////////////
+                              SETUP
+    /////////////////////////////////////////////////////////////// */
+
+    function setUp() public override {
+        AaveLocker_Fork_Test.setUp();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              TESTS
+    //////////////////////////////////////////////////////////////*/
+    function testFork_Success_fullWithdraw(uint256 depositAmount) public {
+        // Given: Deposit a certain amount.
+        depositAmount = bound(depositAmount, 1e18, 10_000 * 1e18);
+        vm.prank(EUREFund);
+        EURE.transfer(address(EURB), depositAmount);
+
+        vm.startPrank(address(EURB));
+        EURE.approve(address(AAVE_LOCKER), depositAmount);
+        AAVE_LOCKER.deposit(address(EURE), depositAmount);
+
+        vm.warp(block.timestamp + 365 days);
+        uint256 initTotalValue = AAVE_LOCKER.getTotalValue(address(EURE));
+
+        // When: Calling fullWithdraw.
+        (uint256 principal, uint256 yield) = AAVE_LOCKER.fullWithdraw(address(EURE));
+        uint256 expectedYield = initTotalValue - depositAmount;
+
+        assertEq(expectedYield, yield);
+        assertEq(EURE.balanceOf(address(EURB)), yield + principal);
+        assertEq(principal, depositAmount);
+
+        vm.stopPrank();
+    }
+}
